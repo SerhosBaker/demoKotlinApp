207061994e543bef6e7fc16f9ada5ce5f0978bac
s.b.markov <s.b.markov@tinkoff.ru>
2025-08-04 14:29:19 +0300

    db integrated

--- src/main/kotlin/domain/pie/PieController.kt
+++ src/main/kotlin/domain/pie/PieController.kt
@@ -17,11 +17,13 @@ import java.time.LocalDateTime
 @HttpController
 class PieController(private val pieRepository: PieRepository) {
 
-    @HttpRoute(method = POST, path = "/pies/meet")
+    // Hello world веб приложения. Контроллер работает, отдает принятые параметры в ответе
+    @HttpRoute(method = GET, path = "/pies/meet")
     fun getPie(@Query weight: Int, @Query orderedFor: String): String {
         return MeetPie(weight = weight, orderedFor = orderedFor, bakedAt = LocalDateTime.now(), id = "1").toString();
     }
 
+    // Проверка работы с БД
     @Mapping(PieResponseMapper::class)
     @HttpRoute(method = GET, path = "/pies/all/{orderedFor}")
     fun getPies(@Path("orderedFor") orderedFor: String): List<String> {
